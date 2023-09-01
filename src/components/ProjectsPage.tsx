9ec76283eb12e84e413512a3485e4de1b6e6d7de
georgievmv <96878439+georgievmv@users.noreply.github.com>
2023-09-01 20:37:48 +0300

    commit

--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -4,16 +4,16 @@ import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const ProjectsPage = () => {
-  const imageMouseOutHandler = () => {
-    const image = document.getElementById('NLB-image');
+  const imageMouseOutHandler = (id: string) => {
+    const image = document.getElementById(id);
     if (image) {
       image.style.objectPosition = '0% 0%';
       image.style.transition = 'object-position 2s';
     }
   };
 
-  const imageMouseEnterHandler = () => {
-    const image = document.getElementById('NLB-image');
+  const imageMouseEnterHandler = (id: string) => {
+    const image = document.getElementById(id);
     if (image) {
       image.style.objectPosition = '0% 100%';
       image.style.transition = 'object-position 5s ease-in';
@@ -54,8 +54,8 @@ const ProjectsPage = () => {
           <a className={styles.imageLink} target="_blank" href="https://nolangbarriers.com/">
             <img
               id="NLB-image"
-              onMouseEnter={imageMouseEnterHandler}
-              onMouseOut={imageMouseOutHandler}
+              onMouseEnter={imageMouseEnterHandler.bind(this, 'NLB-image')}
+              onMouseOut={imageMouseOutHandler.bind(this, 'NLB-image')}
               className={styles.image}
               src="assets\nolangbarriers.com_.png"
               alt=""
@@ -78,6 +78,42 @@ const ProjectsPage = () => {
               </a>
             </div>
           </article>
+        </section>
+      </div>
+      <div className={styles.container}>
+        <section className={styles.card}>
+          <article className={styles.text}>
+            <h2>AROMA HARBOR</h2>
+            <p className={styles.paragraph}>
+              Discover your signature scent with our fragrance decant website. Explore a curated
+              collection of premium perfume samples, perfect for exploring and finding your ideal
+              fragrance.
+            </p>
+            <div className={styles.buttons}>
+              <a target="_blank" href="https://github.com/georgievmv/fragrance-decant-ecommerce">
+                <FontAwesomeIcon icon={faGithub} />
+                <p className={styles.buttonText}>Code</p>
+              </a>
+              <a target="_blank" href="https://georgievmv.github.io/fragrance-decant-ecommerce/">
+                <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                <p className={styles.buttonText}>Visit site</p>
+              </a>
+            </div>
+          </article>
+          <a
+            className={`${styles.imageLink} ${styles.aromaHarbor}`}
+            target="_blank"
+            href="https://georgievmv.github.io/fragrance-decant-ecommerce/"
+          >
+            <img
+              id="AromaHarborImg"
+              onMouseEnter={imageMouseEnterHandler.bind(this, 'AromaHarborImg')}
+              onMouseOut={imageMouseOutHandler.bind(this, 'AromaHarborImg')}
+              className={styles.image}
+              src="assets\frag-decant.png"
+              alt=""
+            />
+          </a>
         </section>
       </div>
     </div>
